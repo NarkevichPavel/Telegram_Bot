31eef99b88a35a25e4cbb171aa37e61e1f91d699
PavelNarkevich <156112714+PavelNarkevich@users.noreply.github.com>
2024-04-21 02:29:01 +0300

    create filter IsAdmin

---
+++ api/filters/chat_types.py
@@ -0,0 +1,12 @@
+from aiogram.filters import Filter
+from aiogram import Bot, types
+
+from api.middlewares.settings import settings
+
+
+class IsAdmin(Filter):
+    def __init__(self) -> None:
+        pass
+
+    async def __call__(self, message: types.Message, bot: Bot) -> bool:
+        return message.from_user.id in settings.bots.admin_id
